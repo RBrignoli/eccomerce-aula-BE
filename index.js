faf11d0970a58dbe6bcf81ea5f74f278bc58fe94
daniloamorim <professor@SN-363526.edu.fiesc.com.br>
2024-09-09 17:17:28 -0300

    tst

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express()
 const bodyParser = require('body-parser')
 const rotas_produtos = require('./rotas/rotas-produto')
 const clientes = require('./rotas/rotas-clientes')
+const rotas_autenticacao = require('./rotas/rotas-autenticacao')
 
 app.use(bodyParser.json())
-
 app.use('/produtos', rotas_produtos)
 app.use('/clientes', clientes)
+app.use('/auth', rotas_autenticacao)
 
 
-// app.listen(8000)
+
+app.listen(8000)
 
 module.exports = app;
