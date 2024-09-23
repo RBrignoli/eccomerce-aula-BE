028c27f03c210400e2a62f3b5e44714868892551
RBrignoli <ramon.brignoli@edu.sc.senai.br>
2024-09-23 17:00:20 -0300

    add docs

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const app = express()
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+
 const rotas_produtos = require('./rotas/rotas-produto')
 const clientes = require('./rotas/rotas-clientes')
 const rotas_autenticacao = require('./rotas/rotas-autenticacao')
+
+const swaggerDocument = YAML.load('./docs/documentacao.yaml')
 
 app.use(bodyParser.json())
 app.use(cookieParser())
@@ -15,6 +20,7 @@ app.use('/produtos', rotas_produtos)
 app.use('/clientes', clientes)
 app.use('/auth', rotas_autenticacao)
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 
 app.listen(8000)
